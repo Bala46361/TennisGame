c54ecbe461eabcedf8787eff0e6e4d1ffa930b03
Bala <bala_465@rediffmail.com>
2020-02-01 07:32:02 +0530

    Refactored :: Moved the player object to configuringObjectCreation method for readability

--- src/test/java/TennisGame/TennisGameTest.java
+++ src/test/java/TennisGame/TennisGameTest.java
@@ -8,21 +8,23 @@ import org.junit.Test;
 public class TennisGameTest {
 
 	TennisGame tennisGame;
+	Player firstPlayer;
+	Player secondPlayer;
 
 	@Before
 	public void configuringObjectCreation() {
 		tennisGame = new TennisGame();
+		firstPlayer = new Player();
+		secondPlayer = new Player();
 	}
-	
+
 	@Test
 	public void validateObjectInstanceforNotNull() {
 		assertNotNull(tennisGame);
 	}
-	
+
 	@Test
 	public void shouldCreateInstanceForTwoPlayers() {
-		Player firstPlayer = new Player();
-		Player secondPlayer= new Player();
 		assertNotNull(firstPlayer);
 		assertNotNull(secondPlayer);
 	}
