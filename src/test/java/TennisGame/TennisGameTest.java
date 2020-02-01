f97ebcc2346c1a22e5114c19a1b36bb094e1488c
Bala <bala_465@rediffmail.com>
2020-02-01 06:51:45 +0530

    Validated:: Able to create object for TennisGame class

---
+++ src/test/java/TennisGame/TennisGameTest.java
@@ -0,0 +1,12 @@
+package TennisGame;
+
+import org.junit.Before;
+
+public class TennisGameTest {
+
+	@Before
+	public void configuringObjectCreation() {
+		TennisGame tennisGame = new TennisGame();
+	}
+
+}
